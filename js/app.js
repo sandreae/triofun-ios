2cf7e8fb491ff53efa233c7e624681083d281bfd
Sam Andreae <sandreae@gmail.com>
2018-03-07 12:19:17 +0100

    touchstart and end for all samples

--- js/app.js
+++ js/app.js
@@ -20,21 +20,30 @@ document.addEventListener("DOMContentLoaded", function() {
         cracked.unlock()
   		})
 
-  		drum1.addEventListener('touchstart', function(event) {
+      drum1.addEventListener('touchstart', function(event) {
         this.classList.add("active")
-  			__("#bass").adsr("trigger")
-  			setInterval(function() {event.target.classList.remove("active")}, 500)
-  		})
-  		drum2.addEventListener('touchstart', function(event) {
+        __("#kick").adsr("trigger")
+      })
+      drum1.addEventListener('touchend', function(event) {
+        this.classList.remove("active")
+        __("#kick").adsr("release")
+      })
+      drum2.addEventListener('touchstart', function(event) {
         this.classList.add("active")
-  			__("#kick").adsr("trigger")
-  			setInterval(function() {event.target.classList.remove("active")}, 500)
-  		})
-  		drum3.addEventListener('touchstart', function(event) {
+        __("#bass").adsr("trigger")
+      })
+      drum2.addEventListener('touchend', function(event) {
+        this.classList.remove("active")
+        __("#bass").adsr("release")
+      })
+      drum3.addEventListener('touchstart', function(event) {
         this.classList.add("active")
-  			__("#snare").adsr("trigger")
-  			setInterval(function() {event.target.classList.remove("active")}, 500)
-  		})
+        __("#snare").adsr("trigger")
+      })
+      drum3.addEventListener('touchend', function(event) {
+        this.classList.remove("active")
+        __("#snare").adsr("release")
+      })
       drum4.addEventListener('touchstart', function(event) {
         this.classList.add("active")
         __("#hihat").adsr("trigger")
@@ -44,9 +53,9 @@ document.addEventListener("DOMContentLoaded", function() {
   			__("#hihat").adsr("release")
   		})
 
-  		__().sine({frequency:180}).adsr({id:"bass",envelope:0.8}).lowpass(120).compressor({release:0}).dac();
-  		__().sine(80).adsr({id:"kick",envelope:0.5}).connect("compressor"); //100ms envelope
-  		__().pink().adsr({id:"snare",envelope:0.3}).connect("compressor"); //50ms
+  		__().sine({frequency:180}).adsr({id:"bass",envelope:[0.1, 0.1, 1]}).lowpass(120).compressor({release:0}).dac();
+  		__().sine(80).adsr({id:"kick",envelope:[0.1, 0.1, 1]}).connect("compressor"); //100ms envelope
+  		__().pink().adsr({id:"snare",envelope:[0.1, 0.1, 1]}).connect("compressor"); //50ms
   		__().white().adsr({id:"hihat",envelope:[0.1, 0.1, 1]}).connect("compressor").play(); //10ms
 
   		cracked.soundLoaded = function(x) {
