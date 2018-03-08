99a2c5353655c323f63a28ea8bffdb801aecd30a
Sam Andreae <sandreae@gmail.com>
2018-03-08 09:10:20 +0100

    sample start point

--- js/app.js
+++ js/app.js
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function() {
   		__().pink().adsr({id:"snare",envelope:[0.1, 0.1, 1]}).connect("compressor");
   		__().white().adsr({id:"hihat",envelope:[0.1, 0.1, 1]}).connect("compressor");
       __().monosynth().connect("compressor").play();
-      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum10", loop:true}).gain({id: "samplergain"})connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum10", loop:true}).gain({id: "samplergain"}).connect("dac");
 
   		cracked.soundLoaded = function(x) {
         console.log("sample loaded", x)
