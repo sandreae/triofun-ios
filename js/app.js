09c713018233f8538e8aa8453832db0fbb1fddb4
Sam Andreae <sandreae@gmail.com>
2018-03-08 09:18:51 +0100

    sample start point

--- js/app.js
+++ js/app.js
@@ -76,16 +76,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
       drum1.addEventListener('touchstart', function(event) {
         this.classList.add("active")
-        __("sampler").start()
+        __("#drum1").start()
       })
   		drum1.addEventListener('touchend', function(event) {
-        this.classList.remove("active")
-        __("#samplergain").ramp([1,0],[0.1])
-        setTimeout(function(){ 
-          __("sampler").stop()
-          __("#samplergain").attr({"gain": 1}) }, 100);
+        var self = this
+        stopSample(self)
   		})
 
+      var stopSample = function(self){
+        var id = "#" + self.id
+        self.classList.remove("active")
+        __(id).ramp([1,0],[0.1])
+        setTimeout(function(){ 
+          __(id).stop()
+          __("#samplergain").attr({"gain": 1}) }, 100);
+      }
 
 
   		__().sine({frequency:180}).adsr({id:"bass",envelope:[0.1, 0.1, 1]}).lowpass(120).compressor({release:0}).dac();
@@ -93,6 +98,15 @@ document.addEventListener("DOMContentLoaded", function() {
   		__().pink().adsr({id:"snare",envelope:[0.1, 0.1, 1]}).connect("compressor");
   		__().white().adsr({id:"hihat",envelope:[0.1, 0.1, 1]}).connect("compressor");
       __().monosynth().connect("compressor").play();
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum1", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum2", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum3", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum4", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum5", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum6", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum7", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum8", loop:true}).gain({id: "samplergain"}).connect("dac");
+      __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum9", loop:true}).gain({id: "samplergain"}).connect("dac");
       __().sampler({path:"/triofun-ios/samples/10_Trtomds.wav", id: "drum10", loop:true}).gain({id: "samplergain"}).connect("dac");
 
   		cracked.soundLoaded = function(x) {
